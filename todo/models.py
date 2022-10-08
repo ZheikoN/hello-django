efaaa99d68a434d317dfc751ac777741f0157e48
ZheikoN <negru.inger@gmail.com>
2022-10-08 11:37:09 +0000

    add delete and edit forms

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+    date = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
